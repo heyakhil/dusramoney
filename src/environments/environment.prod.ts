abb9f22b077f47b1914677239eac93c6a019c101
heyakhil <54666562+heyakhil@users.noreply.github.com>
2022-04-22 21:58:37 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'https://v1.trustedmalls.in/api'
+  apiUrl:'https://v1.dusramoney.in/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:'https://v1.trustedmalls.in/api'
+  apiUrl:'https://v1.dusramoney.in/api'
 };
 
 /*
